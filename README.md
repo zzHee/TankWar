19194fa18317c039022e545d36405ea8f0bb557f
zzHee <772575675@qq.com>
2020-10-22 19:30:31 +0800

    add directions

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 http://note.youdao.com/noteshare?id=9e66d51b23c6f8f0fe96e0deb54a2867&sub=A3DB4995847341D1A2C4160C94E4C57A
+
+
+ 一个java文件中可以定义多个类，但是最多只有一个类被public修饰，并且这个类的类名与文件名必须相同。若这个文件中没有public的类，则文件名随便是一个类的名字即可。
--- src/com/tank/TankFrame.java
+++ src/com/tank/TankFrame.java
@@ -7,7 +7,11 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class TankFrame extends Frame{
+
     private int x = 200, y = 200;
+    Dir dir = Dir.DOWN;
+    private int SPEED = 10;
+
     public TankFrame(){
         setSize(800, 600);
         setResizable(false);
@@ -30,6 +34,22 @@ public class TankFrame extends Frame{
         System.out.println("paint");
         //窗口左上角为原点，横轴为x (width)，纵轴为y (height)。
         g.fillRect(x, y, 50, 100);
+        switch (dir) {
+            case LEFT:
+                x -= SPEED;
+                break;
+            case RIGHT:
+                x += SPEED;
+                break;
+            case UP:
+                y -= SPEED;
+                break;
+            case DOWN:
+                y += SPEED;
+                break;
+            default:
+                break;
+        }
     }
 
     class MyKeyListener extends KeyAdapter {
@@ -37,7 +57,17 @@ public class TankFrame extends Frame{
         boolean br = false;
         boolean bu = false;
         boolean bd = false;
-
+        private void setMainTankDir() {
+            if (bl) {
+                dir = Dir.LEFT;
+            } else if (br) {
+                dir = Dir.RIGHT;
+            } else if (bu) {
+                dir = Dir.UP;
+            } else if (bd) {
+                dir = Dir.DOWN;
+            }
+        }
         @Override
         //这里是系统发现按键事件KeyEvent之后自动调用keyPressed方法，并把KeyEvent事件传入
         public void keyPressed(KeyEvent e) {
@@ -45,41 +75,42 @@ public class TankFrame extends Frame{
             int key = e.getKeyCode();
             switch (key) {
                 case KeyEvent.VK_LEFT:
-                    bl = true;
+                    dir = Dir.LEFT;
                     break;
                 case KeyEvent.VK_RIGHT:
-                    br = true;
+                    dir = Dir.RIGHT;
                     break;
                 case KeyEvent.VK_UP:
-                    bu = true;
+                    dir = Dir.UP;
                     break;
                 case KeyEvent.VK_DOWN:
-                    bd = true;
+                    dir = Dir.DOWN;
                     break;
                 default:
                     break;
             }
-            if (bl && bu) {
-                x -= 10;
-                y -= 10;
-            } else if (bl && bd) {
-                x -= 10;
-                y += 10;
-            } else if (br && bu) {
-                x += 10;
-                y -= 10;
-            } else if (br && bd) {
-                x += 10;
-                y += 10;
-            } else if (bl) {
-                x -= 10;
-            } else if (br) {
-                x += 10;
-            } else if (bu) {
-                y -= 10;
-            } else if (bd) {
-                y += 10;
-            }
+            setMainTankDir();
+//            if (bl && bu) {
+//                x -= 10;
+//                y -= 10;
+//            } else if (bl && bd) {
+//                x -= 10;
+//                y += 10;
+//            } else if (br && bu) {
+//                x += 10;
+//                y -= 10;
+//            } else if (br && bd) {
+//                x += 10;
+//                y += 10;
+//            } else if (bl) {
+//                x -= 10;
+//            } else if (br) {
+//                x += 10;
+//            } else if (bu) {
+//                y -= 10;
+//            } else if (bd) {
+//                y += 10;
+//            }
 
 
             //这支画笔只有系统自己拿得到，所以我们不能直接调用paint(),而使用repaint()代替
@@ -106,6 +137,7 @@ public class TankFrame extends Frame{
                 default:
                     break;
             }
+            setMainTankDir();
         }
     }
 }
